b50f7afec07289aebc32fdec374beb8cf85e1f3d
Andres Cordova <acordova12@bloomberg.net>
2025-11-14 01:50:47 +0000

    First commit

---
+++ mysite/settings.py
@@ -0,0 +1,7 @@
+SECRET_KEY = 'django-insecure-dev-key'
+DEBUG = True
+ALLOWED_HOSTS = ['*']
+INSTALLED_APPS = []
+ROOT_URLCONF = 'urls'
+MIDDLEWARE = []
+TEMPLATES = []
---
+++ urls.py
@@ -0,0 +1,7 @@
+from django.http import HttpResponse
+from django.urls import path
+
+def hello(request):
+    return HttpResponse("Hello, World!")
+
+urlpatterns = [path('', hello)]
